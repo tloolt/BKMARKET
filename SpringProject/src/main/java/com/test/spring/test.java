17070d21787e3a8d84da0bae170b0b3a82749987
BAE <BAE@DESKTOP-BJU929Q>
2017-10-15 15:16:16 +0900

    .

--- SpringProject/src/main/java/com/test/spring/test.java
+++ SpringProject/src/main/java/com/test/spring/test.java
@@ -5,11 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("테스트입니다dsadassad.");
-		System.out.println("�븞�뀞�븯�꽭�슂�뀋�꽮�꽩�꽮�뀋�꽩�꽮�꽩�뀋�꽮�뀋�꽩�꽩�뀋...");
-		System.out.println("�븞�뀞�븯�꽭�슂�뀋�꽮�꽩�꽮�뀋�꽩�꽮�꽩�뀋�꽮�뀋�꽩�꽩�뀋...");
-
 		System.out.println("한글");
-
 		System.out.println("한글 테스트	");
 		
 		
