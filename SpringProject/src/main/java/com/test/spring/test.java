afab7bcfab708b3146b49c74adb21bfcb42b2edb
kim <kim@DESKTOP-KSV74A1>
2017-10-15 15:04:04 +0900

    .

--- SpringProject/src/main/java/com/test/spring/test.java
+++ SpringProject/src/main/java/com/test/spring/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("테스트입니다.");
+		System.out.println("테스트입니다dsadassad.");
 
 	}
 
