2706cd7002df462f82a053bac92b73c2a7faf8b6
BAE <BAE@DESKTOP-BJU929Q>
2017-10-15 17:59:25 +0900

    dddd

--- SpringProject/src/main/java/com/test/spring/test.java
+++ SpringProject/src/main/java/com/test/spring/test.java
@@ -5,8 +5,6 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("테스트입니다dsadassad.");
-		System.out.println("한글");
-		System.out.println("한글 테스트	");
 		System.out.println("키키키키키킹");
 		System.out.println("테스트");
 		
@@ -14,21 +12,8 @@ public class test {
 		System.out.println("테스트22"+2);
 		System.out.println("테스트22"+2); 
 		System.out.println("test");
-<<<<<<< HEAD
-=======
-		System.out.println("테스트22"+2);
-<<<<<<< HEAD
->>>>>>> branch 'master' of https://github.com/tloolt/BKMARKET.git
-=======
-		System.out.println("테스트22"+2);
-		System.out.println("테스트22"+2);
-<<<<<<< HEAD
->>>>>>> branch 'master' of https://github.com/tloolt/BKMARKET.git
-=======
-		System.out.println("테스트22"+2);
 		System.out.println("테스트22"+2);
 		
->>>>>>> branch 'master' of https://github.com/tloolt/BKMARKET.git
 		
 
 	}
