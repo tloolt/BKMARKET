06a7d89a6940125d73675ebcedf1db893857bd0b
kim <kim@DESKTOP-KSV74A1>
2017-10-15 15:12:52 +0900

    .

--- SpringProject/src/main/java/com/test/spring/test.java
+++ SpringProject/src/main/java/com/test/spring/test.java
@@ -7,6 +7,7 @@ public class test {
 		System.out.println("테스트입니다dsadassad.");
 		System.out.println("�븞�뀞�븯�꽭�슂�뀋�꽮�꽩�꽮�뀋�꽩�꽮�꽩�뀋�꽮�뀋�꽩�꽩�뀋...");
 		System.out.println("�븞�뀞�븯�꽭�슂�뀋�꽮�꽩�꽮�뀋�꽩�꽮�꽩�뀋�꽮�뀋�꽩�꽩�뀋...");
+		System.out.println("한글");
 
 	}
 
