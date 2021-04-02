c0f134400ef9791770ece17426955943b47155db
이명훈 <mhlee7558@interpark.com>
2021-04-02 13:07:45 +0900

    111111111111111111111333333333333333

--- SpringProject/src/main/java/com/test/spring/HomeController.java
+++ SpringProject/src/main/java/com/test/spring/HomeController.java
@@ -23,7 +23,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/h", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		//logger.info("Welcome home! The client locale is {}.", locale);
+		//2logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
