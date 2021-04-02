649dd0db6a23d7a6f482105369cf637cd2b223fb
이명훈 <mhlee7558@interpark.com>
2021-04-02 12:50:53 +0900

    TEST SERVICE

--- SpringProject/src/main/java/com/test/spring/test.java
+++ SpringProject/src/main/java/com/test/spring/test.java
@@ -12,7 +12,7 @@ public class test {
 		System.out.println("테스트22"+2);
 		System.out.println("테스트22"+2); 
 		System.out.println("test");
-		System.out.println("테스트22"+2);
+		//System.out.println("테스트22"+2);
 		
 		
 
